89e0bde31d7c961f58b47eae434c56654c142d6b
shovon0203 <shovon0203@yahoo.com>
2024-06-05 12:37:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 def main():
     secret_pass = os.getenv('PASS')
     print(f"Secret PASS: {secret_pass}")
-    my_output = f"Hello! Your secret is {secret_pass}"
+    my_output = f"Your secret is {secret_pass}"
     print(f"::set-output name=my-output::{my_output}")
 
 if __name__ == "__main__":
