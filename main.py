f2022e19e756a2231850524fdcb80feaa4d0275c
shovon0203 <shovon0203@yahoo.com>
2024-06-05 10:55:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 
 def main():
-    my_input = os.getenv('MY_INPUT', 'default value')
     secret_pass = os.getenv('PASS')
-    print(f"Input received: {my_input}")
     print(f"Secret PASS: {secret_pass}")
-    my_output = f"Hello, {my_input}! Your secret is {secret_pass}"
+    my_output = f"Hello! Your secret is {secret_pass}"
     print(f"::set-output name=my-output::{my_output}")
 
 if __name__ == "__main__":
