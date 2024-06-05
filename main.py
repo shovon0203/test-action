64f3643e5614cd7d1f352077e85e2f0238174faf
shovon0203 <shovon0203@yahoo.com>
2024-06-05 00:15:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
-import sys
 
 def main():
-    my_input = sys.argv[1]
-    print(f"Input received: {my_input}")
-    my_output = f"Hello, {my_input}!"
+    secret_pass = os.getenv('PASS')
+    print(f"Secret PASS: {secret_pass}")
+    my_output = f"Hello, Your secret is {secret_pass}"
     print(f"::set-output name=my-output::{my_output}")
 
 if __name__ == "__main__":
