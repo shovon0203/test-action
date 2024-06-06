c56cf065a507e3e5b81236209406f1e5fc7cdff9
shovon0203 <shovon0203@yahoo.com>
2024-06-06 11:05:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 
 def main():
     secret_pass = os.getenv('PASS')
-    print(f"Secret PASS: {secret_pass}")
-    my_output = f"Your secret is {secret_pass}"
-    print(f"::set-output name=my-output::{my_output}")
+    if secret_pass:
+        print(f"Secret PASS: {secret_pass}")
+        my_output = f"Your secret is {secret_pass}"
+        print(f"::set-output name=my-output::{my_output}")
+    else:
+        print("No secret PASS found.")
 
 if __name__ == "__main__":
     main()
+
 
